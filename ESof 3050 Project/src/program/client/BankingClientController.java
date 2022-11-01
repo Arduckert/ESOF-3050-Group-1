13905931dafdd31913ddc112f18b91a102606668
Connor <Connor@DESKTOP-GC69VFT.lakeheadu.ca>
2022-11-01 10:54:22 -0400

    ocsf framework and client/server program beginning

---
+++ ESof 3050 Project/src/program/client/BankingClientController.java
@@ -0,0 +1,10 @@
+package program.client;
+
+public class BankingClientController
+{
+	public static void main(String args[])
+	{
+		System.out.println("testing the client");
+	}
+
+}
---
+++ ESof 3050 Project/src/program/server/BankController.java
@@ -0,0 +1,9 @@
+package program.server;
+
+public class BankController
+{
+	public static void main(String args[])
+	{
+		System.out.println("testing the server");
+	}
+}
