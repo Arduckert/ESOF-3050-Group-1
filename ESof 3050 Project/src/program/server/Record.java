fde233baa677b51b864ce69824a1b95718b8a6f6
manta <manta@LAPTOP-DFOTTR1O.lakeheadu.ca>
2022-11-07 11:35:01 -0500

    gsg

--- ESof 3050 Project/src/program/server/Record.java
+++ ESof 3050 Project/src/program/server/Record.java
@@ -19,8 +19,8 @@ public abstract class Record {
 	public String getRecordDate() {
 		return this.recordDate;
 	}
-	@Override
-	public String toString() {
-		return String.format("Date: %s\nTeller: %s %s\n", getRecordDate(),getTeller().getFName(),getTeller().getLName());
-	}
+	//@Override
+	//public String toString() {
+		//return String.format("Date: %s\nTeller: %s %s\n", getRecordDate(),getTeller().getFName(),getTeller().getLName());
+	//}
 }
