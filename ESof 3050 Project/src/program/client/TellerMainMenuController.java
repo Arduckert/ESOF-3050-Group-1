8b0dfea86564d121ed77c49e5b7ced91a9fe947d
mattc <mattc@LAPTOP-69C69K5U.lakeheadu.ca>
2022-11-07 13:55:48 -0500

    This is the first test of GUI components

---
+++ ESof 3050 Project/src/program/client/TellerMainMenuController.java
@@ -0,0 +1,22 @@
+package program.client;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class TellerMainMenuController {
+
+    @FXML
+    void CreateNewAccountHolderButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LogoutButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SearchExistingAccountHolderButtonPressed(ActionEvent event) {
+
+    }
+
+}
