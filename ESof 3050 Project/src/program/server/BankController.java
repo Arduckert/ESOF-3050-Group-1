fff8f7e0d984c160746c3bfc86914fafdb820db4
manta <manta@LAPTOP-DFOTTR1O.lakeheadu.ca>
2022-11-07 15:45:29 -0500

    yyy

--- ESof 3050 Project/src/program/server/BankController.java
+++ ESof 3050 Project/src/program/server/BankController.java
@@ -8,12 +8,37 @@ public class BankController
 	List<Account> accountList = new ArrayList<Account>();
 	double generalInterestRate;
 	
+	//setter
 	public void setGeneralInterestRate(double x) {
 		this.generalInterestRate = x;
 	}
+	//getter
 	public double getGeneralInterestRate() {
 		return this.generalInterestRate;
 	}
+	
+	//search functions
+	public Person searchPerson(int sin) {
+		for(int i=0; i<personList.size(); i++) {
+			if(personList.get(i).getSIN() == sin) {
+				return personList.get(i);
+			}
+		}
+		return null; //throw error maybe
+	}
+	
+	public Account searchAccount(int num) {
+		for(int i=0; i<accountList.size(); i++) {
+			if(accountList.get(i).getAccountNum() == num) {
+				return accountList.get(i);
+			}
+		}
+		return null; //throw error maybe
+	}
+	
+	
+	
+	
 	public static void main(String args[])
 	{
 		System.out.println("testing the server...");
--- ESof 3050 Project/src/program/server/Teller.java
+++ ESof 3050 Project/src/program/server/Teller.java
@@ -1,14 +1,14 @@
 package program.server;
 
-public class Teller{
+public class Teller extends personRole{
 	private int empNum;
 	private String password;
 	
 	//Constructor
-	public Teller(int x, String y) {
+	public Teller(int x, String y, Person person) {
+		super(1, person);
 		this.empNum = x;
 		this.password = y;
-		
 	}
 	//Getters
 	public int getEmpNum() {
