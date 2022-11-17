d94da774f3af9acbb0bcac9861e02534425bd2f8
Connor <Connor@10.0.0.157>
2022-11-17 12:26:37 -0500

    the protocol code didn't merge for some reason pt1

--- ESof 3050 Project/src/program/client/BankingClient.java
+++ ESof 3050 Project/src/program/client/BankingClient.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.SocketException;
 
 import src.ocsf.client.AbstractClient;
+import src.protocol.*;
 
 public class BankingClient extends AbstractClient
 {
@@ -13,17 +14,23 @@ public class BankingClient extends AbstractClient
 	}
 
 	@Override
-	protected void handleMessageFromServer(Object msg) {
-		// TODO Auto-generated method stub
-		System.out.println(msg);
+	protected void handleMessageFromServer(Object msg)
+	{
+		ServerProtocol sp = (ServerProtocol)msg.getClass().cast(msg);
+		System.out.println(sp.GetData().get(0));
 	}
 	
 	@Override
 	public void sendToServer(Object msg) throws IOException {
 		System.out.println("Trying to send: " + msg);
+		System.out.println("Host: "+super.getHost());
+		System.out.println("Port: "+super.getPort());
 		super.sendToServer(msg);
-		System.out.println("Success");
-		
 	}
 	
+	public void SendTestMessageToServer(String message) throws IOException
+	{
+		ClientProtocol cp = new ClientProtocol(ServerAction.TEST, message);
+		sendToServer(cp);
+	}	
 }
