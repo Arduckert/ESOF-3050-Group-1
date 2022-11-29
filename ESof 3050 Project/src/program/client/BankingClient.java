5ac1f2703d602609e443aa065b6f20863ee3e4f9
Connor <Connor@DESKTOP-GC69VFT.lakeheadu.ca>
2022-11-29 10:42:18 -0500

    commit test

--- ESof 3050 Project/src/program/client/BankingClient.java
+++ ESof 3050 Project/src/program/client/BankingClient.java
@@ -8,19 +8,38 @@ import src.protocol.*;
 
 public class BankingClient extends AbstractClient
 {
+	//abstract banking controller interface. An interface is used here to
+	//accompany both the actual bank controller and a test driver to test
+	//the banking client
 	private IBankingClientController bcc;
 	
-	public BankingClient(String host, int port, IBankingClientController bcc) {
+	/**
+	 * Creates a new banking client
+	 * @param host the host's IP address
+	 * @param port the host's port
+	 * @param bcc an instance of a banking client controller
+	 */
+	public BankingClient(String host, int port, IBankingClientController bcc)
+	{
 		super(host, port);
 		this.bcc = bcc;
-		// TODO Auto-generated constructor stub
 	}
-
+	
+	/**
+	 * This function is responsible for decoding the object as a server protocol
+	 * and determining which function to call based on the data type included
+	 * within the ServerProtocol object
+	 */
 	@Override
 	protected void handleMessageFromServer(Object msg)
 	{
+		//casts the incoming object as a serverProtocol object
 		ServerProtocol sp = (ServerProtocol)msg;
 		
+		//determines which function to call based on the data type
+		//included in the ServerProtool object. Additional processing
+		//is done within these functions before they are passed to
+		//the banking client controller for analysis
 		switch(sp.getDataType())
 		{
 			case LOGIN_ATTEMPT:
@@ -33,8 +52,13 @@ public class BankingClient extends AbstractClient
 		}
 	}
 	
+	/**
+	 * Sends an object to the server.
+	 * @implNote the object MUST be of type ClientProtocol
+	 */
 	@Override
-	public void sendToServer(Object msg) throws IOException {
+	public void sendToServer(Object msg) throws IOException
+	{
 		System.out.println("Sending message to " + super.getHost() + ":" + super.getPort());
 		super.sendToServer(msg);
 	}
@@ -82,19 +106,26 @@ public class BankingClient extends AbstractClient
 		sendToServer(cp);
 	}
 	
+	/**
+	 * If the message from the server is a basic message, the client will
+	 * print the message to the console
+	 * @param sp serverProtocol passed in by the server
+	 */
 	private void ProcessBasicMessage(ServerProtocol sp)
 	{
-		System.out.println(sp.GetData().get(0));
+		//passes the string message from the server protocol to the
+		//banking client controller instance
+		bcc.HandleBasicMessage(sp.GetData().get(0));
 	}
 	
 	// TODO: setup code for login attempt
 	private void ProcessLoginAttempt(ServerProtocol sp)
 	{
 		if(sp.getMessageStatus() == MessageStatus.SUCCESS) {
-			//switch scene
+			//TODO: switch scene
 		}
 		else {
-			//pop up
+			//TODO: pop up
 		}
 	}
 }
