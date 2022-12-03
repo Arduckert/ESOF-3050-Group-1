6a69721d1247f082fe4f5f3adac19ee4385d750e
manta <manta@192.168.250.44>
2022-12-03 12:35:01 -0500

    Completed all back-end server classes

--- ESof 3050 Project/src/program/server/Record.java
+++ ESof 3050 Project/src/program/server/Record.java
@@ -4,31 +4,31 @@ import java.time.LocalDate;
 public abstract class Record {
 	private LocalDate recordDate;
 	private Teller teller;
-	private String type;
 	private String action;
 	
-	public Record(Teller teller, String t, String a) {
+	public Record(Teller teller, String a) {
 		this.recordDate = LocalDate.now();
 		this.teller = teller;
-		this.type = t;
 		this.action = a;
 	}
 	public Teller getTeller() {
 		return this.teller;
 	}
-	/*
+	
 	public String getRecordDate() {
 		return String.format("%s",recordDate);
 	}
 	
+	@Override
 	public String toString() {
-		if(type == "account") {
-			switch(action) {
-			case "created":
-				return String.format("ACCOUNT CREATED\nTeller:%s %s", getRecordDate(),getTeller().getFName(),getTeller().getLName());
-			}
+		switch(action) {
+		case "created":
+			return String.format("%s: Created",recordDate);
+		case "removed":
+			return String.format("%s: Removed",recordDate);
+		default:
+			return String.format("");
 		}
-		
 	}
-	*/
+
 }
