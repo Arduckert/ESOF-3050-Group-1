d86d9b9a3e4717036dcc836758bc8d5d8b4b59e3
RedHeadDJ <84481421+DJGame321@users.noreply.github.com>
2022-12-07 01:37:01 -0500

    fixed a slight oof in the get transaction history code

--- ESof 3050 Project/src/program/server/BankingServer.java
+++ ESof 3050 Project/src/program/server/BankingServer.java
@@ -733,7 +733,7 @@ public class BankingServer extends AbstractServer
 		if (client.getInfo("LoginType") == LoginType.ACCOUNTHOLDER)
 		{
 			//gets all the transactions from the banking server
-			ArrayList<TransactionInfo> info = bc.getTransactionHistory((String)client.getInfo("AccNum"));
+			ArrayList<TransactionInfo> info = bc.getTransactionHistory(cp.GetParameters().get(0));
 			
 			ServerProtocol sp = new ServerProtocol(MessageStatus.SUCCESS, Datatype.TRANSACTION);
 			
